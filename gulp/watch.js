cf27a8f205e12cb1d2ab1e26fdd1f75cffcd839a
Jesper Håkansson <jh222xk@student.lnu.se>
2015-03-15 17:02:36 +0100

    [Chore] Remove haml

--- gulp/watch.js
+++ gulp/watch.js
@@ -4,12 +4,11 @@ var gulp = require('gulp');
 
 var paths = gulp.paths;
 
-gulp.task('watch', ['markups', 'inject'], function () {
+gulp.task('watch', ['inject'], function () {
   gulp.watch([
     paths.src + '/*.html',
     paths.src + '/**/*.scss',
     paths.src + '/**/*.js',
     'bower.json'
   ], ['inject']);
-  gulp.watch(paths.src + '/**/*.haml', ['markups']);
 });
